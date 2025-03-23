d6d9ce103a55f07191921abc5a25d59981fded3e
Sumeet416 <sumeet.17758@sakec.ac.in>
2025-03-23 23:12:13 +0530

    templates and static directory

--- Myproject/Myproject/views.py
+++ Myproject/Myproject/views.py
@@ -5,7 +5,9 @@ def home(Request):
     return render( Request , 'index.html')
 
 def about(Request):
-    return HttpResponse("Hello World, you are at about page")
+    # return HttpResponse("Hello World, you are at about page")
+    return render(Request , 'about.html')
 
 def contact(Request):
-    return HttpResponse("Hello World, you are at contact page")
+    # return HttpResponse("Hello World, you are at contact page")
+    return render(Request , 'contact.html')
