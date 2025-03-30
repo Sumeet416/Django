9d1986cd209eee7e0d7a5be333f9c44068d71b3d
Sumeet416 <sumeet.17758@sakec.ac.in>
2025-03-30 23:05:31 +0530

    initializing models in django

--- Myproject/App/models.py
+++ Myproject/App/models.py
@@ -1,3 +1,16 @@
 from django.db import models
-
+from django.utils import timezone
 # Create your models here.
+class ClassVarity(models.Model):
+    USERS =[
+        ('AS', 'ASPIRANT'),
+        ('RT', 'RECRUITER'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField( upload_to='images/')
+    date = models.DateTimeField(default=timezone.now)
+    user = models.CharField(max_length=2, choices = USERS)
+
+def __str__(self):
+    return self.name
+
