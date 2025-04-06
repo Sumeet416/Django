9d9979e80b5b3e4c74d2c415d502bcc8967182d6
Sumeet416 <sumeet.17758@sakec.ac.in>
2025-04-06 22:53:05 +0530

    Reviews error

--- Myproject/Chai/models.py
+++ Myproject/Chai/models.py
@@ -18,4 +18,21 @@ class ChaiVarity(models.Model):
 
     def __str__(self):
         return self.name
+
+class Reviews(models.Model):
+    RATING_CHOICES = [
+        (1, '1 Star'),
+        (2, '2 Stars'),
+        (3, '3 Stars'),
+        (4, '4 Stars'),
+        (5, '5 Stars'),
+    ]
+    chai_varity = models.ForeignKey(ChaiVarity, on_delete=models.CASCADE, related_name='reviews')
+    name = models.CharField(max_length=100)
+    review = models.TextField()
+    rating = models.IntegerField(choices=RATING_CHOICES)
+    created_at = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f'{self.name} - {self.chai_varity.name}'
     
